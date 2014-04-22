bc0d9b129169512066784e4dda741023449484a4
Amaury Coulomban <amaury.coulomban@insa-lyon.fr>
2014-04-22 11:33:33 +0200

    Interface main 	Boutons 	Background

--- shopmylifi/src/com/example/shopmylifi/MainActivity.java
+++ shopmylifi/src/com/example/shopmylifi/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.shopmylifi;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,62 +10,64 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.os.Build;
+import android.widget.ImageButton;
 
 public class MainActivity extends ActionBarActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+	//	final ImageButton itineraireButton = (ImageButton) findViewById(R.id.main_button_itineraire);
+	//	final ImageButton carteButton = (ImageButton) findViewById(R.id.main_button_map);
+		final ImageButton listeButton = (ImageButton) findViewById(R.id.main_button_liste_course);
+		listeButton.setOnClickListener(new OnClickListener() {
 
-        final Button loginButton = (Button) findViewById(R.id.main_button_liste_course);
-        loginButton.setOnClickListener(new OnClickListener() {
-      			
-        @Override
-        public void onClick(View v) {
-      	Intent intent = new Intent(MainActivity.this, DisplayListeCourse.class);
-      	startActivity(intent);
-      	}
-      });        
-    }
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this,
+						DisplayListeCourse.class);
+				startActivity(intent);
+			}
+		});
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
 
-        public PlaceholderFragment() {
-        }
+		public PlaceholderFragment() {
+		}
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
+			return rootView;
+		}
+	}
 
 }
