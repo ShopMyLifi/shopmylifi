8127e238f22584ca4ff48a7cda9a73d82585c5a8
Amaury Coulomban <amaury.coulomban@insa-lyon.fr>
2014-04-22 09:01:38 +0200

    2 vues + lien

--- shopmylifi/src/com/example/shopmylifi/MainActivity.java
+++ shopmylifi/src/com/example/shopmylifi/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.shopmylifi;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
@@ -18,11 +21,15 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
+        final Button loginButton = (Button) findViewById(R.id.main_button_liste_course);
+        loginButton.setOnClickListener(new OnClickListener() {
+      			
+        @Override
+        public void onClick(View v) {
+      	Intent intent = new Intent(MainActivity.this, DisplayListeCourse.class);
+      	startActivity(intent);
+      	}
+      });        
     }
 
 
