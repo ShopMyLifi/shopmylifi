acc21da8a2144f9e567fce524555763d5a058c5e
Amaury Coulomban <amaury.coulomban@insa-lyon.fr>
2014-04-23 18:14:50 +0200

    .

--- shopmylifi/src/com/example/shopmylifi/DisplayListeCourse.java
+++ shopmylifi/src/com/example/shopmylifi/DisplayListeCourse.java
@@ -5,16 +5,9 @@ import java.util.HashMap;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.TextView; 
-import android.widget.AdapterView;
 
 import java.util.List;
 
