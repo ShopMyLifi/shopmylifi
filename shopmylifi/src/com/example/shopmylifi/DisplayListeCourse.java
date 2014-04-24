e09695b448d80fa35509fca98b3e2f80ccea23f3
Amaury Coulomban <amaury.coulomban@insa-lyon.fr>
2014-04-24 10:40:50 +0200

    a

--- shopmylifi/src/com/example/shopmylifi/DisplayListeCourse.java
+++ shopmylifi/src/com/example/shopmylifi/DisplayListeCourse.java
@@ -5,8 +5,12 @@ import java.util.HashMap;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.ArrayAdapter;
 
 import java.util.List;
@@ -17,6 +21,17 @@ public class DisplayListeCourse extends Activity{
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 	    setContentView(R.layout.display_liste_course);
+	    
+	    final ImageButton homeButton = (ImageButton) findViewById(R.id.liste_course_button_home);
+		homeButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(DisplayListeCourse.this,
+						MainActivity.class);
+				startActivity(intent);
+			}
+		});
 	
 	    final ListView listview = (ListView) findViewById(R.id.id_display_liste_course);
 	    String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
