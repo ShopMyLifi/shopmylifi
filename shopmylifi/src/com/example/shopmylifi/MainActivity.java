74de748f0c90d28db2f5cd0f7734ba89ec2ba67e
Amaury Coulomban <amaury.coulomban@insa-lyon.fr>
2014-04-25 18:15:24 +0200

    Ajout du motherfucker bandeau de pub en bas de la liste. Sérieux ça m'a pris tout l'aprem pour une erreur conne cette merde.

--- shopmylifi/src/com/example/shopmylifi/MainActivity.java
+++ shopmylifi/src/com/example/shopmylifi/MainActivity.java
@@ -1,15 +1,12 @@
 package com.example.shopmylifi;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.view.Window;
 
@@ -53,23 +50,6 @@ public class MainActivity extends ActionBarActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
-		}
 	}
 
 }
