d52c251b372e8dbc9c14c52e63e946eaf50eb811
Amaury Coulomban <amaury.coulomban@insa-lyon.fr>
2014-05-22 20:53:55 +0200

    Dans ta face BI-BI-BI-BITCH Itinéraire qui marche (normalement, je crois, probablement, peut-être, sans doute) A vérifier sur d'autres listes et des plus longues.
    + UI de base pour itinéraire

--- shopmylifi/src/com/example/itineraire/CalculItineraire.java
+++ shopmylifi/src/com/example/itineraire/CalculItineraire.java
@@ -2,7 +2,7 @@ package com.example.itineraire;
 
 import java.util.Arrays;
 
-import android.util.Log;
+//import android.util.Log;
 
 public class CalculItineraire {
 
@@ -18,37 +18,14 @@ public class CalculItineraire {
 		int matriceN[][] = new int[dim][2];
 
 		initNNAlgo(matriceD, matriceN);
-		
+
+		amelioration2OptAlgo(matriceD, matriceN);
+
 		iti[0] = 0;
-		
-		for (int i = 0; i < dim-1; i++) {
+
+		for (int i = 0; i < dim - 1; i++) {
 			iti[i + 1] = matriceN[iti[i]][NN];
 		}
-				
-		
-		/*
-		 * // meilleur inineraire pour le moment int[] bestNextPlace = new
-		 * int[dim];
-		 * 
-		 * for (int i = 0; i < dim - 1; i++) bestNextPlace[i] = i + 1;
-		 * 
-		 * // itineraire finit en 0 bestNextPlace[dim - 1] = 0;
-		 * 
-		 * int bestCost = findCost(bestNextPlace, matriceD); boolean change =
-		 * true;
-		 * 
-		 * while (change) { int nextPlace[] = new int[dim];
-		 * 
-		 * for (int i = 1; i < dim - 1; i++) for (int j = i + 1; j < dim - 1;
-		 * j++) {
-		 * 
-		 * System.arraycopy(bestNextPlace, 0, nextPlace, 0, dim); int temp =
-		 * nextPlace[i];
-		 * 
-		 * }
-		 * 
-		 * }
-		 */
 	}
 
 	private static void initNNAlgo(int matriceD[][], int matriceN[][]) {
@@ -97,21 +74,99 @@ public class CalculItineraire {
 
 	}
 
-	public static void main(String[] args) {
-		int matrice[][] = { { 0, 8, 6, 10, 7, 16, 16, 16, 4 },
-				{ 8, 0, 8, 4, 13, 8, 8, 8, 12 },
-				{ 6, 8, 0, 4, 5, 10, 16, 10, 4 },
-				{ 10, 4, 4, 0, 9, 6, 12, 6, 8 },
-				{ 7, 13, 5, 9, 0, 15, 21, 9, 5 },
-				{ 16, 8, 10, 6, 15, 0, 6, 6, 14 },
-				{ 16, 8, 16, 12, 21, 6, 0, 12, 21 },
-				{ 16, 8, 10, 6, 9, 6, 12, 0, 14 },
-				{ 4, 12, 4, 8, 5, 14, 21, 14, 0 } };
+	private static void amelioration2OptAlgo(int matriceD[][], int matriceN[][]) {
 
-		int dim = matrice.length;
-		int[] iti = new int[dim + 1];
-		computeIti(matrice, iti);
+		int dim = matriceD.length;
+		int actualBestCost = cost(matriceN);
+		boolean change = true;
 
-	
+		while (change) {
+
+			change = false;
+
+			for (int i = 0; i < dim - 1; i++) {
+				for (int j = i + 1; j < dim; j++) {
+
+					int tempCost;
+					tempCost = changeCost(matriceD, matriceN, actualBestCost,
+							i, j);
+
+					if (tempCost < actualBestCost) {
+
+						actualBestCost = tempCost;
+						apply2Opt(matriceN, i, j);
+						change = true;
+
+					}
+				}
+			}
+		}
 	}
+
+	private static int cost(int matriceN[][]) {
+
+		int dim = matriceN.length;
+		int cost = 0;
+
+		for (int i = 0; i < dim; i++) {
+			cost += matriceN[i][NN];
+		}
+
+		return (cost);
+	}
+
+	private static int changeCost(int[][] matriceD, int[][] matriceN,
+			int actualCost, int point1, int point2) {
+
+		int nextPoint1 = matriceN[point1][NN];
+		int nextPoint2 = matriceN[point2][NN];
+
+		int changeCost = actualCost - matriceD[point1][nextPoint1]
+				- matriceD[point2][nextPoint2] + matriceD[point1][point2]
+				+ matriceD[nextPoint1][nextPoint2];
+		return (changeCost);
+
+	}
+
+	private static void xchangeDirection(int[][] matriceN, int pointCurr,
+			int pointStop) {
+
+		if (pointCurr != pointStop) {
+
+			xchangeDirection(matriceN, matriceN[pointCurr][NN], pointStop);
+
+			int temp = matriceN[pointCurr][NN];
+			matriceN[pointCurr][NN] = matriceN[pointCurr][PN];
+			matriceN[pointCurr][PN] = temp;
+		}
+	}
+
+	private static void apply2Opt(int[][] matriceN, int point1, int point2) {
+
+		xchangeDirection(matriceN, matriceN[point1][NN], point2);
+
+		int nextPoint1 = matriceN[point1][NN];
+		int nextPoint2 = matriceN[point2][NN];
+
+		matriceN[point1][NN] = point2;
+		matriceN[point2][NN] = matriceN[point2][PN];
+		matriceN[nextPoint1][NN] = nextPoint2;
+		matriceN[point2][PN] = point1;
+		matriceN[nextPoint2][PN] = nextPoint1;
+
+	}
+
+	/*
+	 * public static void main(String[] args) { int matrice[][] = { { 0, 8, 6,
+	 * 10, 7, 16, 16, 16, 4 }, { 8, 0, 8, 4, 13, 8, 8, 8, 12 }, { 6, 8, 0, 4, 5,
+	 * 10, 16, 10, 4 }, { 10, 4, 4, 0, 9, 6, 12, 6, 8 }, { 7, 13, 5, 9, 0, 15,
+	 * 21, 9, 5 }, { 16, 8, 10, 6, 15, 0, 6, 6, 14 }, { 16, 8, 16, 12, 21, 6, 0,
+	 * 12, 21 }, { 16, 8, 10, 6, 9, 6, 12, 0, 14 }, { 4, 12, 4, 8, 5, 14, 21,
+	 * 14, 0 } };
+	 * 
+	 * int dim = matrice.length; int[] iti = new int[dim + 1];
+	 * computeIti(matrice, iti);
+	 * 
+	 * }
+	 */
 }
--- shopmylifi/src/com/example/shopmylifi/ItineraireActivity.java
+++ shopmylifi/src/com/example/shopmylifi/ItineraireActivity.java
@@ -3,8 +3,12 @@ package com.example.shopmylifi;
 import java.util.Arrays;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.example.itineraire.CreationMatriceDistance;
@@ -18,15 +22,39 @@ public class ItineraireActivity extends Activity {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.display_itineraire);
 
+		final ImageButton homeButton = (ImageButton) findViewById(R.id.itineraire_button_home);
+		homeButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(ItineraireActivity.this,
+						MainActivity.class);
+				startActivity(intent);
+			}
+		});
+
+		final ImageButton reglagesButton = (ImageButton) findViewById(R.id.itineraire_button_settings);
+		reglagesButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(ItineraireActivity.this,
+						ReglagesActivity.class);
+				startActivity(intent);
+
+			}
+		});
+
 		CreationMatriceDistance creationMatrice = new CreationMatriceDistance();
 
 		int[][] matriceD;
 		matriceD = creationMatrice.calculmatrice();
-		
+
 		int[] iti = new int[matriceD.length];
 		CalculItineraire.computeIti(matriceD, iti);
 
-		Toast.makeText(getApplicationContext(), (Arrays.toString(iti)), Toast.LENGTH_SHORT).show();
+		Toast.makeText(getApplicationContext(), (Arrays.toString(iti)),
+				Toast.LENGTH_SHORT).show();
 	}
 
 }
